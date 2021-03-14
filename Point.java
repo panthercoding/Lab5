f5a5f851bf3bb3aba0cec1d2527fae06eaa0c7db
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-14 15:12:23 -0500

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,40 @@
+public class Point{
+
+  private double x;
+  private double y;
+
+  /* should assign xCoord to x and yCoord to y */
+  public Point(double xCoord, double yCoord){
+    /* to be completed */
+  }
+
+  /* @returns the value of x */
+  public double getX(){
+    /* to be completed */
+  }
+
+  /* @returns the value of y */
+  public double getY(){
+    /* to be completed */
+  }
+
+  /* @param: a new value of x
+  changes the current value of x */
+  public void setX(double newX){
+    /* to be completed */
+  }
+
+  /* @param: a new value of y
+  changes the current value of y */
+  public void setY(double newY){
+    /* to be completed */
+  }
+
+  /* @param: a Point object
+  @return the distance between this Point object
+  and the other Point object provided through input */
+  public double distanceTo(Point other){
+    /* to be completed */
+  }
+
+}
